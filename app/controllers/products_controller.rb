617a98d4a9cfdb8f485184e104744f561213dbf2
Maggie Summers <summers.maggie@gmail.com>
2018-04-20 13:03:28 -0700

    add scope to show products with most reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @most_reviews = @products.most_reviews
   end
 
   def new
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :cost, :country_of_origin, :presence => true
+
+  scope :most_reviews, -> {(
+    select("products.id, products.name, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(5))}
 end
