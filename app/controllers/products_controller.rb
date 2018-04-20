cc3c8b04215f804883fb97f2d775fc7eb7bcbbf5
Maggie Summers <summers.maggie@gmail.com>
2018-04-20 10:50:55 -0700

    add functionality to add new product to products list and database

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product saved successfully!"
+      redirect_to products_path
+    else
+      flash[:alert] = "Sorry, couldn't save."
+      render :new
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :cost, :country_of_origin)
+    end
 end
