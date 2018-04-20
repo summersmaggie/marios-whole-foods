19b78945719395c4f45e59fbf363ba84ce24f943
Maggie Summers <summers.maggie@gmail.com>
2018-04-20 09:28:35 -0700

    add 50 products and 250 reviews via faker seeding gem

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 gem 'bootstrap-sass', '~> 3.3.7'
-
+gem 'faker'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  @product = Product.create!(name: Faker::Food.ingredient,
+                            cost: Faker::Number.decimal(2),
+                            country_of_origin: Faker::Address.country)
+  5.times do
+    Review.create!(author: Faker::RickAndMorty.character,
+                    content_body: Faker::RickAndMorty.quote,
+                    rating: Faker::Number.between(1, 5),
+                    product_id: @product.id)
+  end
+end
+
+p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
