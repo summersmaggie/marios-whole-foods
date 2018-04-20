267d70e5bc24732fd003c6622ad670f1a9d592fd
Maggie Summers <summers.maggie@gmail.com>
2018-04-20 13:47:03 -0700

    add validation for requiring the rating to be an integer

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :content_body, :rating, :presence => true
-  validates :content_body, length: { in: 50..250 } 
+  validates :content_body, length: { in: 50..250 }
+  validates :rating, numericality: { only_integer: true }
 end
