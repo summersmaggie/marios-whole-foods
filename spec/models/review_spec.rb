19ff01bd16aa127ebd66a12c570ed4bcc75d8d3c
Maggie Summers <summers.maggie@gmail.com>
2018-04-23 18:34:13 -0700

    add passing test spec for character count

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,4 +6,9 @@ describe Review do
   it { should validate_presence_of :rating }
 
   it { should belong_to :product }
+
+  it("should have a content body length between 50 and 250 characters") do
+    new_review = Review.new(:author => "Summers", :content_body => "Parks and Rec is a great show.", :rating => 5)
+    expect(new_review.save).to eq(false)
+  end
 end
