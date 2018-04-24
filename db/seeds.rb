0b197770b1b88a77d7bb841c719234c54368e3eb
Maggie Summers <summers.maggie@gmail.com>
2018-04-23 18:14:06 -0700

    update seeds file to seed properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,14 @@ Product.destroy_all
 Review.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Food.ingredient,
+  product = Product.create!(name: Faker::Food.ingredient,
                             cost: Faker::Number.decimal(2),
                             country_of_origin: Faker::Address.country)
-  @product_id = Product.last.id
   5.times do |index|
     Review.create!(author: Faker::RickAndMorty.character,
-                    content_body: Faker::RickAndMorty.quote,
+                    content_body: Faker::Lorem.characters(75),
                     rating: Faker::Number.between(1, 5),
-                    product_id: @product_id)
+                    product_id: product.id)
   end
 end
 
